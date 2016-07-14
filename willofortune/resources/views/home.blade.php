9a274b091b2f905cadd98b71ca6ad552d6b984de
Elie Ishimwe <elie.ishimwe@ignitiongroup.co.za>
2016-07-14 08:15:43 +0200

    updates

--- willofortune/resources/views/home.blade.php
+++ willofortune/resources/views/home.blade.php
@@ -76,7 +76,10 @@
                             </div>
                       </li>
 
-                       <li>
+                      
+
+                      @endif
+                      <li>
 
                             <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
                               <div class="tile-stats">
@@ -87,8 +90,6 @@
                               </div>
                             </div>
                       </li>
-
-                      @endif
                     </ul>
                     
                     
